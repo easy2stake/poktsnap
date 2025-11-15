dfd7802ff029139d62c33e15f290a8906a9bf803
Alex <alex@easy2stake.com>
2025-11-15 22:47:11 +0200

    Update entrypoint.sh

--- standalone/entrypoint.sh
+++ standalone/entrypoint.sh
@@ -196,9 +196,7 @@ fi
 
 # Cleanup and exit
 echo "[entrypoint] Shutting down node..."
-kill -SIGINT $PPD_PID 2>/dev/null
-sleep 5
-wait $PPD_PID 2>/dev/null
+kill $PPD_PID 2>/dev/null
 
 echo "[entrypoint] Done!"
 exit 0
